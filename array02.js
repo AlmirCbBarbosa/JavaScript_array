e2e624cd70792f4ecd443b5936e1d82b49ac228a
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2023-08-21 20:21:42 -0300

    voltando o estado

--- array02.js
+++
@@ -1,9 +0,0 @@
-/*utilizar um método que adiciona uma elemento no final de uma array*/
-
-let notas1 = [10.5, 7.8 , 11.2];
-
-console.log(notas1);
-console.log('\n');
-
-notas1.push(2.5);
-console.log(notas1);
