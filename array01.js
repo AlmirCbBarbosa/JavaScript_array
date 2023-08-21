72479c7e41b01f759fb17aaa037639c59a08f37d
AlmirCbBarbosa <almirdelimaestudos@gmail.com>
2023-08-21 17:48:22 -0300

    Inicializando e inserindo elemento na array

---
+++ array01.js
@@ -0,0 +1,7 @@
+/*Este arquivo tem como objetivo explorar algumas maneiras de iniciar arrays */
+
+let notas1 = new Array(10.5, 11.2, 15.0); //não é uma boa prática
+let notas2 = [10.5, 11.2, 15.0 ]; // boa prática iniciar desta maneira
+
+console.log(notas1);
+console.log(notas2);
